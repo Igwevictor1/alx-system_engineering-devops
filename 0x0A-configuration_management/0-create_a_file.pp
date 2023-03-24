ca6a83ffb91b911a4d3a9ced52ec02376538e945
victor <igwevictor11@yahoo.com>
2023-03-24 09:46:44 -0700

    Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# puppet create a file
-
-file { '/tmp':
-  ensure  => file,
-  path    => '/tmp/school',
+# A puppet manifest that creates a file school in /tmp.
+# The file (School) has permissions 0744, belonging to the user and group
+# www-data.
+file { '/tmp/school':
+  ensure  => present,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# puppet install a package
-
-package { 'puppet-lint':
-  ensure  => present,
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
+# Am also specifying the version of flask to install using the ensure attribute
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
 }
